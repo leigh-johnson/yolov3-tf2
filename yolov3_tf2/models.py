144d21fba081591e0912161b80847f61f98a27b6
Jason Zaman <jason@perfinion.com>
2020-06-19 13:40:39 -0700

    TFLite export with builtin-ops for TF-2.2
    The resize-bilinear op was missing support for half-pixel-centers:
    https://github.com/tensorflow/tensorflow/issues/33691
    
    TFLite export with only the builtin-ops with TF-2.2 (or with that fix
    backported to TF-2.1). The MLIR converter is also required to support
    the NMS layers.
    
    Signed-off-by: Jason Zaman <jason@perfinion.com>

--- yolov3_tf2/models.py
+++ yolov3_tf2/models.py
@@ -105,7 +105,7 @@ def YoloConv(filters, name=None):
 
             # concat with skip connection
             x = DarknetConv(x, filters, 1)
-            x = UpSampling2D(2)(x)
+            x = UpSampling2D(2, interpolation='bilinear')(x)
             x = Concatenate()([x, x_skip])
         else:
             x = inputs = Input(x_in.shape[1:])
@@ -127,7 +127,7 @@ def YoloConvTiny(filters, name=None):
 
             # concat with skip connection
             x = DarknetConv(x, filters, 1)
-            x = UpSampling2D(2)(x)
+            x = UpSampling2D(2, interpolation='bilinear')(x)
             x = Concatenate()([x, x_skip])
         else:
             x = inputs = Input(x_in.shape[1:])
